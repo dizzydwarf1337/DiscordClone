4f4c90c7973c03336f1cb86b653d249ff291e613
Anton Krymouski <86262@student.pb.edu.pl>
2024-10-24 19:59:42 +0200

    Minor changes

--- DiscordClone/client-app/src/app/layout/navBar.tsx
+++ DiscordClone/client-app/src/app/layout/navBar.tsx
@@ -1,11 +1,19 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import "./styles.css";
 export default function NavBar() {
     return (
         <>
-            <AppBar sx={{ backgroundColor: "#3C3C3C", zIndex:3, pl:"50px"}}>
-                <Toolbar >
-                    <Typography>Discord Clone</Typography>
+            <AppBar sx={{ backgroundColor: "#3C3C3C", zIndex: 3, pl: "50px" }}>
+                <Toolbar sx={{ justifyContent: "space-between" }}>
+                    <Box className="leftSideNavBox">
+                        <Typography>Discord Clone</Typography>
+                    </Box>
+
+
+
+                    <Box className="rightSideNavBox">
+                        Profile Settings
+                    </Box>
                 </Toolbar>
             </AppBar>
         </>
